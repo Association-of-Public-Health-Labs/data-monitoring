d0b610759c16d81c9a0f1cd2c5fdbe83c21448fa
Vagner Pene <vagnerpene@gmail.com>
2021-02-15 19:12:45 +0200

    Update websocket: add 'disconnect', 'osinfo', 'servers'

--- src/websocket.js
+++ src/websocket.js
@@ -5,14 +5,44 @@ module.exports = {
     const io = socketio(server);
 
     io.on("connection", function (socket) {
-      console.log(socket.id);
-      socket.on("osinfo", function (socket) {
-        console.log(socket);
-      });
-    });
+      const {server_id} = socket;
+      const connectedServers = [];
+      const osinfo = [];
 
-    // io.on("cpu_usage", function (socket) {
-    //   console.log(socket);
-    // });
+      if(server_id){
+        connectedServers[server_id] = socket.id;
+        io.emit("connectedServers", {
+          connectedServers,
+          osinfo
+        })
+      }
+
+      socket.on("disconnect", socket => {
+        connectedServers[server_id] = null;
+        osinfo[server.server_id] = null
+        io.emit("connectedServers", {
+          connectedServers,
+          osinfo
+        })
+      })
+
+      socket.on("osinfo", function (server) {
+        osinfo[server.server_id] = {
+          server_id: server.server_id,
+          cpu: server.cpu,
+          ram: server.ram,
+          sqlagent: server.sqlagent,
+        }
+      });
+
+      var interval = 1000;
+
+      setInterval(async function () {
+        io.emit("servers", {
+          connectedServers,
+          osinfo
+        })
+      }, interval);
+    });
   },
 };
